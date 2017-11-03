e68042ed325a63f7be1b11fd024b2f03d0a07a02
takasan989 <d.0508taka@gmail.com>
2017-11-03 17:15:16 +0900

    赤黒木途中

---
+++ search/red_black_tree/red_black_tree.go
@@ -0,0 +1,279 @@
+package red_black_tree
+
+import (
+	"fmt"
+)
+
+type color int
+
+const (
+	BLACK color = iota
+	RED
+)
+
+type Tree struct {
+	root *Node
+}
+
+type Node struct {
+	tree   *Tree
+	color  color
+	parent *Node
+	left   *Node
+	right  *Node
+	value  int
+}
+
+func NewTree() *Tree {
+	tree := Tree{}
+	return &tree
+}
+
+func NewNode() *Node {
+	node := Node{}
+	return &node
+}
+
+func (node *Node) grand() *Node {
+	parent := node.parent
+	
+	if parent == nil {
+		return nil
+	}
+	
+	return parent.parent
+}
+
+func (node *Node) uncle() *Node {
+	grand := node.grand()
+	
+	if grand == nil {
+		return nil
+	}
+	
+	if grand.left == node.parent {
+		return grand.right
+	} else {
+		return grand.left
+	}
+}
+
+func (node *Node) balance() {
+	parent := node.parent
+	grand := node.grand()
+	uncle := node.uncle()
+	
+	if node.tree != nil || parent.color == BLACK {
+		node.balanceCase1()
+		return
+	}
+	
+	if uncle.color == RED {
+		node.balanceCase2()
+		return
+	}
+	
+	if (grand.left == parent && parent.right == node) || (grand.right == parent && parent.left == node) {
+		node.balanceCase3()
+		return
+	}
+	
+	if (grand.left == parent && parent.left == node) || (grand.left == parent && parent.left == node) {
+		node.balanceCase4()
+		return
+	}
+	
+	fmt.Errorf("invalid state")
+}
+
+func (node *Node) balanceCase4() {
+	parent := node.parent
+	grand := node.grand()
+	
+	if grand.left == parent {
+		grand.rotateRight()
+	} else {
+		grand.rotateLeft()
+	}
+	
+	grand.color, parent.color = parent.color, grand.color
+}
+
+func (node *Node) balanceCase3() {
+	parent := node.parent
+	grand := node.grand()
+	
+	if grand.left == parent {
+		parent.rotateLeft()
+		parent.balance()
+	} else {
+		parent.rotateRight()
+		parent.balance()
+	}
+}
+
+func (node *Node) balanceCase2() {
+	parent := node.parent
+	grand := node.grand()
+	uncle := node.uncle()
+	
+	parent.color = BLACK
+	uncle.color = BLACK
+	
+	grand.color = RED
+	grand.balance()
+}
+
+func (node *Node) balanceCase1() {
+	if node.tree != nil {
+		node.color = BLACK
+		return
+	}
+	
+	node.color = RED
+}
+
+func (tree *Tree) Append(value int) {
+	if tree.root == nil {
+		tree.root = &Node{value: value, tree: tree}
+		return
+	}
+	
+	tree.root.append(&Node{value: value, color: RED})
+}
+
+func (tree *Tree) append(node *Node) *Node {
+	if tree.root == nil {
+		tree.root = node
+		node.tree = tree
+		return node
+	}
+	
+	return tree.root.append(node)
+}
+
+func (node *Node) Append(value int) {
+	if value < node.value {
+		if node.left != nil {
+			node.left.Append(value)
+			return
+		}
+		node.left = &Node{value: value, parent: node}
+		return
+	}
+	
+	if node.right != nil {
+		node.right.Append(value)
+		return
+	}
+	node.right = &Node{value: value, parent: node}
+}
+
+func (parent *Node) append(node *Node) *Node {
+	if node.value < parent.value {
+		if parent.left != nil {
+			return parent.left.append(node)
+		}
+		parent.left = node
+		node.parent = parent
+		return node
+	}
+	
+	if parent.right != nil {
+		return parent.right.append(node)
+	}
+	
+	parent.right = node
+	node.parent = parent
+	return node
+}
+
+func (node *Node) rotateLeft() {
+	if node.tree != nil {
+		right := node.right
+		node.right = right.left
+		if right.left != nil {
+			right.left.parent = node
+		}
+		
+		right.tree = node.tree
+		right.tree.root = right
+		right.parent = nil
+		node.tree = nil
+		
+		right.left = node
+		node.parent = right
+		return
+	}
+	
+	right := node.right
+	node.right = right.left
+	if right.left != nil {
+		right.left.parent = node
+	}
+	
+	right.parent = node.parent
+	if node.parent.left == node {
+		node.parent.left = right
+	} else {
+		node.parent.right = right
+	}
+	
+	right.left = node
+	node.parent = right
+}
+
+func (node *Node) rotateRight() {
+	if node.tree != nil {
+		left := node.left
+		node.left = left.right
+		if left.right != nil {
+			left.right.parent = node
+		}
+		
+		left.tree = node.tree
+		left.tree.root = left
+		left.parent = nil
+		node.tree = nil
+		
+		left.right = node
+		node.parent = left
+		return
+	}
+	
+	left := node.left
+	node.left = left.right
+	if left.right != nil {
+		left.right.parent = node
+	}
+	
+	left.parent = node.parent
+	if node.parent.left == node {
+		node.parent.left = left
+	} else {
+		node.parent.right = left
+	}
+	
+	left.right = node
+	node.parent = left
+}
+
+func (tree *Tree) String() string {
+	return fmt.Sprintf("Tree{%s}", tree.root.String())
+}
+
+func (node *Node) String() string {
+	value := node.value
+	
+	left := ""
+	if node.left != nil {
+		left = fmt.Sprintf(",left: %s", node.left)
+	}
+	
+	right := ""
+	if node.right != nil {
+		right = fmt.Sprintf(",right: %s", node.right)
+	}
+	
+	return fmt.Sprintf("Node{%d%s%s}", value, left, right)
+}
---
+++ search/red_black_tree/red_black_tree_test.go
@@ -0,0 +1,134 @@
+package red_black_tree
+
+import (
+	"testing"
+)
+
+func TestBalance(t *testing.T) {
+	tree := &Tree{}
+	
+	for i := 10; i > 0; i-- {
+		tree.Append(i)
+	}
+	
+	t.Errorf("%v", tree)
+}
+
+func TestBalanceCase2(t *testing.T) {
+	tree := &Tree{}
+	g := tree.append(&Node{value:100, color: BLACK})
+	p := tree.append(&Node{value:50, color: RED})
+	u := tree.append(&Node{value:150, color: RED})
+	n := tree.append(&Node{value:25, color: RED})
+	
+	n.balanceCase2()
+	
+	if p.color != BLACK || u.color != BLACK || g.color != BLACK {
+		t.Errorf("balanceCase2 %v", p.color)
+		t.Errorf("balanceCase2 %v", u.color)
+		t.Errorf("balanceCase2 %v", g.color)
+	}
+}
+
+func TestBalanceCase1(t* testing.T) {
+	tree := &Tree{}
+	node := tree.append(&Node{value:10, color: RED})
+	
+	node.balanceCase1()
+	
+	if node.color != BLACK {
+		t.Errorf("balanceCase1 %v", node)
+	}
+	
+	node = tree.append(&Node{value:5, color: RED})
+	
+	node.balanceCase1()
+	
+	if node.color != RED {
+		t.Errorf("balanceCase1 %v", node)
+	}
+}
+
+func TestRotateLeft(t *testing.T) {
+	tree := &Tree{}
+	tree.Append(3)
+	tree.Append(2)
+	tree.Append(5)
+	tree.Append(4)
+	tree.Append(6)
+	tree.root.rotateLeft()
+	
+	actual := tree.String()
+	expected := "Tree{Node{5,left: Node{3,left: Node{2},right: Node{4}},right: Node{6}}}"
+	
+	if actual != expected {
+		t.Errorf("rotateLeft %v", actual)
+	}
+}
+
+func TestRotateRight(t *testing.T) {
+	tree := NewTree()
+	tree.Append(5)
+	tree.Append(6)
+	tree.Append(3)
+	tree.Append(2)
+	tree.Append(4)
+	tree.root.rotateRight()
+	
+	actual := tree.String()
+	expected := "Tree{Node{3,left: Node{2},right: Node{5,left: Node{4},right: Node{6}}}}"
+	
+	if actual != expected {
+		t.Errorf("rotateRight %v", actual)
+	}
+}
+
+func TestString(t *testing.T) {
+	tree := NewTree()
+	tree.Append(2)
+	tree.Append(1)
+	tree.Append(3)
+	
+	actual := tree.String()
+	expected := "Tree{Node{2,left: Node{1},right: Node{3}}}"
+	
+	if actual != expected {
+		t.Errorf("String %s", actual)
+	}
+}
+
+func TestAppendToNewTree(t *testing.T) {
+	tree := NewTree()
+	tree.Append(1)
+	
+	if tree.root.value != 1 {
+		t.Errorf("Append %v", tree)
+	}
+}
+
+func TestAppendToTree(t *testing.T) {
+	tree := NewTree()
+	tree.Append(2)
+	tree.Append(1)
+	tree.Append(3)
+	
+	if tree.root.left.value != 1 || tree.root.right.value != 3 {
+		t.Errorf("Append %v", tree)
+	}
+}
+
+func TestNewTree(t *testing.T) {
+	tree := NewTree()
+	
+	if tree.root != nil {
+		t.Errorf("NewTree %v", tree)
+	}
+}
+
+func TestNewNode(t *testing.T) {
+	root := NewNode()
+
+	if root.color != BLACK || root.left != nil || root.right != nil {
+		t.Errorf("NewNode error %v", root)
+	}
+}
